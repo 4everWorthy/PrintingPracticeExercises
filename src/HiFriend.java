01e971871945c487e6ffbafa87c68adffb0aa645
Jason Worthy <kjasonworthy@gmail.com>
2024-07-22 16:39:36 -0400

    "Vacation"

---
+++ src/HiFriend.java
@@ -0,0 +1,7 @@
+public class HiFriend {
+    public static void main(String[] args) {
+        String firstName = "Larry";
+        String lastName = "Sprinkle";
+        System.out.println("Hi " + firstName + " " + lastName + "!");
+    }
+}
